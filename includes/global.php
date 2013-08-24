fdd06abfae35c732af686a141fd91dd1c07922c6
shri12491 <shri12491@gmail.com>
2013-08-24 22:13:24 +0530

    fixed watch list query

--- includes/global.php
+++ includes/global.php
@@ -29,9 +29,19 @@ elseif(isset($_COOKIE['id_cookie'])){
 $session_id=$_COOKIE['id_cookie'];
 $session_username=$_COOKIE['username_cookie'];
 $session_pass=$_COOKIE['pass_cookie'];
-
+$query=mysql_query("SELECT * FROM membership.members WHERE id='$session_id' AND password='$session_pass' LIMIT 1") or die("Could not check the session");
 				$count_count=mysql_num_rows($query);
-if($count_count > 0){$loggedin=1;}
+				//w list track
+				$q = mysql_query("SELECT id FROM membership.subscribers WHERE username='$session_username'") or die("failed to read userdata from db".mysql_error());;
+                $w_count=mysql_num_rows($q);
+				while($wrow=mysql_fetch_array($q)){$w_list[]=$wrow[0];}//w list track
+				
+if($count_count > 0){while($row=mysql_fetch_array($query)){$session_username=$row['username'];}
+						$_SESSION['id']=$session_id;
+						$_SESSION['username']=$session_username;
+						$_SESSION['pass']=$session_pass;
+						$_SESSION['w_count']=$w_count;
+						$loggedin=1;}
 else{
 header("Location:login.php");
 exit(); 
